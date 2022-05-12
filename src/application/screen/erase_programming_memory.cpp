dce7d0b7362da6bb9368906f917984631c9e5c0d
TurboD <turbod@localhost>
2022-05-12 11:05:07 +0200

    On erase EEPROM, save OSCCAL

--- src/application/screen/erase_programming_memory.cpp
+++ src/application/screen/erase_programming_memory.cpp
@@ -19,10 +19,12 @@ namespace screen {
       input::lcd->setCursor(0, 0);
       input::lcd->print("Erasing...");
 
-      // get OSCCAL value
+      // get CONF WORD and OSCCAL value from BKP
       input::pic->init();
+      input::pic->send_command(pic12f509::CMD_READ_DATA);
+      pic12f509::word_t conf_word = input::pic->get_word();
       input::pic->send_command(pic12f509::CMD_INCREMENT_ADDRESS);
-      for (pic12f509::addr_t i = 0; i < pic12f509::LOC_RESET_VECTOR ; i++) {
+      for (pic12f509::addr_t i = 0; i < pic12f509::LOC_OSCCAL_VALUE ; i++) {
         input::pic->send_command(pic12f509::CMD_INCREMENT_ADDRESS);
       }
       input::pic->send_command(pic12f509::CMD_READ_DATA);
@@ -34,8 +36,12 @@ namespace screen {
       input::pic->send_command(pic12f509::CMD_BULK_ERASE);
       input::pic->end();
 
-      // set OSCCAL value
+      // set CONF_WORD and OSCCAL value
       input::pic->init();
+      input::pic->send_command(pic12f509::CMD_LOAD_DATA);
+      input::pic->send_word(conf_word);
+      input::pic->send_command(pic12f509::CMD_BEGIN_PROGRAMMING);
+      input::pic->send_command(pic12f509::CMD_END_PROGRAMMING);
       input::pic->send_command(pic12f509::CMD_INCREMENT_ADDRESS);
       for (pic12f509::addr_t i = 0; i < pic12f509::LOC_RESET_VECTOR ; i++) {
         input::pic->send_command(pic12f509::CMD_INCREMENT_ADDRESS);
